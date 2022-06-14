1105ee363bbcaae178c360e6ae10b6ee77830012
miutamihai <miuta.mihai@gmail.com>
2022-06-14 11:41:52 +0300

    [master] Add get user cart products procedure

---
+++ priv/repo/migrations/20220614084119_add_get_user_cart_products_procedure.exs
@@ -0,0 +1,19 @@
+defmodule DanubianTrade.Repo.Migrations.AddGetUserCartProductsProcedure do
+  use Ecto.Migration
+
+  def change do
+    execute """
+      create or replace procedure get_user_cart_products(
+      user_id_input int
+      )
+      begin
+      select p.name, p.image, cp.quantity, p.price * cp.quantity as total
+      from products as p
+               inner join cart_products cp on p.id = cp.product_id
+               inner join carts c on cp.cart_id = c.id
+      where c.user_id = user_id_input;
+      end;
+    """
+
+  end
+end
